313e38b294d41a7655d3d93cce8d3af53f7e5972
Passeri Matteo <capobanda@gmail.com>
2024-10-20 19:05:29 +0200

    FEAT: flatten every tree structure directories

--- Main.py
+++ Main.py
@@ -1,8 +1,10 @@
 import os
 import shutil
 
-FOLDERS_TO_FIX = os.path.join(os.getcwd(), 'FoldersToFix')
+FOLDERS_TO_FIX = os.path.join(os.getcwd(), 'test/FoldersToFix')
 print('Folders to Fix:', FOLDERS_TO_FIX)
+
+
 
 # for each folder in the current directory,
 for folder in os.listdir(FOLDERS_TO_FIX):
@@ -10,19 +12,30 @@ for folder in os.listdir(FOLDERS_TO_FIX):
     # if it's a folder,
     folder_path = os.path.join(FOLDERS_TO_FIX, folder)
     if os.path.isdir(folder_path):
-        # for each subfolder in the folder,
-        for subfolder in os.listdir(folder_path):
-            #print('Subfolder:', subfolder)
-            subfolder_path = os.path.join(folder_path, subfolder)
-            # if the subfolder is a directory,
-            if os.path.isdir(subfolder_path):
-                # move the files from the subfolder to the directory and delete the subfolder;
-                # if the file exist in the directory, overwrite it;
-                for file in os.listdir(subfolder_path):
-                    shutil.move(os.path.join(subfolder_path, file), os.path.join(folder_path, file))
-                print('Files Moved.')
-                os.rmdir(subfolder_path)
-                print('Subfolder Deleted:', subfolder)                
+
+        for dirpath, _, filenames in os.walk(folder_path, topdown=False):
+            for filename in filenames:
+                i = 0
+                source = os.path.join(dirpath, filename)
+                target = os.path.join(folder_path, filename)
+
+                while os.path.exists(target):
+                    i += 1
+                    file_parts = os.path.splitext(os.path.basename(filename))
+
+                    target = os.path.join(
+                        folder_path,
+                        file_parts[0] + "_" + str(i) + file_parts[1],
+                    )
+
+                shutil.move(source, target)
+
+                print("Moved ", source, " to ", target)
+
+            if dirpath != folder_path:
+                os.rmdir(dirpath)
+
+                print("Deleted ", dirpath)             
 
         # if the folder name contains '+_+', replace it with ' - ';
         new_folder = folder.replace('+_+', ' - ')
